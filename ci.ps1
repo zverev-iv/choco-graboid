0176e9a424aa7cb6d1f97a48c8ba321c25c76fb9
zverev iv <mail@zverev-iv.me>
2020-12-14 18:02:01 +0300

    fix key param

--- ci.ps1
+++ ci.ps1
@@ -1,26 +1,30 @@
 Param (
-	[Parameter(Mandatory=$false, Position=1)]
+	[Parameter(Mandatory = $false, Position = 1)]
 	[bool] $staticAnalysis = $false,
-	[Parameter(Mandatory=$false, Position=2)]
+	[Parameter(Mandatory = $false, Position = 2)]
 	[bool] $build = $false,
-	[Parameter(Mandatory=$false, Position=3)]
-	[bool] $deploy = $false
+	[Parameter(Mandatory = $false, Position = 3)]
+	[bool] $deploy = $false,
+	[Parameter(Mandatory = $false, Position = 4)]
+	[bool] $publishKey = $null
 )
 
-if(!$staticAnalysis -and !$build -and $deploy) {
+if (!$staticAnalysis -and !$build -and $deploy) {
 	Write-Output "Nothing to do."
 	return 0
 }
 
-if($staticAnalysis) {
+if ($staticAnalysis) {
 	Invoke-ScriptAnalyzer "$((Get-ChildItem .\*\tools\*.ps1).FullName)"
 }
 
-if($build) {
+if ($build) {
 	choco pack "$((Get-ChildItem .\*\*.nuspec).FullName)"
 }
 
-if($deploy) {
-	choco apikey --key $CHOCOAPIKEY --source https://push.chocolatey.org/
+if ($deploy -and $publishKey) {
+	choco apikey --key $publishKey --source https://push.chocolatey.org/
 	choco push
+} elseif ($deploy) {
+	Write-Output "please provide a publishKey"
 }
